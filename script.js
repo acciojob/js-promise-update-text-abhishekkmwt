78c6ed9d7bbcedeb96b212384414bc25f042d987
Abhishek Kumawat <65283365+abhishekkmwt@users.noreply.github.com>
2023-06-26 17:12:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function a(){
+	return new Promise((resolve, reject)=>{
+		setTimeout(()=>{
+			resolve("Hello, world!");
+		},1000)
+	})
+}
+
+a().then((data)=>{
+	const ele = document.getElemetById("output");
+	ele.innerText="data";
+	div.appendChild(ele);
+})
